5f99662bbc96603d1eb8cdb094af5b4cea1d9c8e
Hwisaek <hwisaek@gmail.com>
2024-07-14 02:20:40 +0900

    지뢰찾기 시간 초 변경 구현

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/driver/desktop"
 	"log"
 	"mouse-macro/hook"
 	"mouse-macro/layout"
@@ -19,23 +18,13 @@ func main() {
 	}
 
 	myApp.SetIcon(fyne.NewStaticResource("icon.png", icon))
-	myWindow := myApp.NewWindow("Mouse macro")
-	myWindow.Resize(fyne.Size{
-		Width:  300,
-		Height: 550,
-	})
+
+	myWindow := myApp.NewWindow("mouse-macro")
+	myWindow.Resize(fyne.Size{Width: 300, Height: 550})
 	myWindow.SetFixedSize(false)
 
 	layout.Init(myWindow)
 	hook.Init()
-
-	if desk, ok := myApp.(desktop.App); ok {
-		m := fyne.NewMenu("MyApp",
-			fyne.NewMenuItem("Show", func() {
-				log.Println("Tapped show")
-			}))
-		desk.SetSystemTrayMenu(m)
-	}
 
 	myWindow.ShowAndRun()
 }
