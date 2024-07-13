fc67494c91d14ef7334d064dbf83f34a2c7ff649
Hwisaek <hwisaek@gmail.com>
2024-07-14 03:20:44 +0900

    memory read write 분리

--- process/memory/read.go
+++ process/memory/read.go
@@ -1,1 +1,16 @@
 package memory
+
+import (
+	"golang.org/x/sys/windows"
+	"mouse-macro/process"
+	"unsafe"
+)
+
+func ReadMemory(address uintptr) (data uint32, bytesRead uintptr) {
+	err := windows.ReadProcessMemory(process.Handle, address, (*byte)(unsafe.Pointer(&data)), unsafe.Sizeof(data), &bytesRead)
+	if err != nil {
+		return
+	}
+
+	return
+}
